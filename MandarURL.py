9d3af0832d5850352397ade35c5ea6f33e344ff0
MHJGuillermo <50417471+MHJGuillermo@users.noreply.github.com>
2019-07-10 11:22:32 -0500

    Update MandarURL.py

--- MandarURL.py
+++ MandarURL.py
@@ -1,15 +1,14 @@
 from flask import Flask
 import sys
 #import subprocess
-import ListaRecUsuariosparaCasas20190709
-
+import ListaRecUsOb
 app = Flask(__name__)
 #import os 
 #os.system('python prueba.py')
 
 @app.route('/Recomendacion/<num_casa>')
 def Recomendacion(num_casa):
-    s2_out = ListaRecUsuariosparaCasas20190709.main(num_casa)
+    s2_out = ListaRecUsOb.main(num_casa)
     return s2_out
 
 if __name__ == '__main__':
